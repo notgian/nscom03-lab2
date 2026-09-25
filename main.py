53603f8f3101afa67d6f4fc0e521f41630c37b24
Gian <85060753+notgian@users.noreply.github.com>
2026-09-25 18:44:05 +0800

    removed unused mcolors import

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import matplotlib
 matplotlib.use('Agg')  # Set non-interactive backend before pyplot
 
 import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 import numpy as np
 import os
 
